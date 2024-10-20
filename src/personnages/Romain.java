ec25b9d6a0c7abc8e714abfbb1959875947f7ac1
Olivier Gibert <ogib31@gmail.com>
2024-10-20 20:13:49 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,6 +4,8 @@ public class Romain {
 
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipements = 0;
 
 	private boolean isInvariantVerified(int force) {
 	    return force > 0;
@@ -17,9 +19,10 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
 		assert isInvariantVerified(this.force);
 	}
-
+	
 	public String getNom() {
 		return nom;
 	}
@@ -56,6 +59,22 @@ public class Romain {
 		return "Romain [nom=" + nom + ", force=" + force + "]";
 	}
 	
+	private void sEquiper(Equipement equipement) {
+		if (nbEquipements >= 2) {
+			parler("est déjà bien protégé !");
+			return;
+		}
+		for(int i=0;i<nbEquipements;i++) {
+			if(equipements[i] == equipement) {
+				parler("possède déjà un " + equipement + " !");
+				return;
+			}
+		}
+		equipements[nbEquipements] = equipement;
+		nbEquipements ++;
+		parler("s'équipe avec " + equipement + ".");
+	}
+	
 	public static void main(String[] args) {
 		Romain garde = new Romain("Garde",6);
 		Gaulois asterix = new Gaulois("Asterix",8);
@@ -69,5 +88,9 @@ public class Romain {
 //		Romain garde2 = new Romain("Garde2",-6);
 //		garde2.recevoirCoup(-1);
 		
+		garde.sEquiper(Equipement.CASQUE);
+		garde.sEquiper(Equipement.CASQUE);
+		garde.sEquiper(Equipement.BOUCLIER);
+		garde.sEquiper(Equipement.CASQUE);
 	}
 }
